aef521075b69eadb522f08673db34ec3c6bdb9ca
DaveBett <davide.betterini@gmail.com>
2025-03-24 11:20:25 +0000

    polished code

--- substrings.rb
+++ substrings.rb
@@ -1,22 +1,21 @@
+# frozen_string_literal: true
+
 def substrings(string, dictionary)
-  #takes a word as the first argument and then an array of valid substrings as the second argument. 
-  #it should return a hash listing each substring that was found in the original string and how many times it was found.
+  # takes a word as the first argument and then an array of valid substrings as the second argument.
+  # it returns a hash listing each substring that was found in the original string and how many times it was found.
 
   result = {}
-  #makes it case insesitive
+  # makes it case insesitive
   string_lowercase = string.downcase
 
-  #iterates through each word in the dictionary to find correspondance
+  # iterates through each word in the dictionary to find correspondance
   dictionary.each do |word|
     counter = string_lowercase.scan(word.downcase).length
-    #if it finds one, it adds it to the counter and stores how many times each word was found in the value of the hash
-    if counter > 0
-      result[word] = counter
-    end
+    # if it finds one, it adds it to the counter and stores how many times each word was found in the value of the hash
+    result[word] = counter if counter.positive?
   end
-  return result
+  result
 end
 
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-puts substrings("below", dictionary)
-
+dictionary = %w[below down go going horn how howdy it i low own part partner sit]
+puts substrings('below', dictionary)
